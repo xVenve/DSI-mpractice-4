d93b9d777a8ff2d1b1569bf2cf4bf1296d18e143
Jorge Rodríguez <jrf1616@gmail.com>
2021-04-18 21:45:57 +0200

    Minipractica 4 1.4

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('micropractica4 app is running!');
+    expect(page.getTitleText()).toEqual('practica04 app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'micropractica4'`, () => {
+  it(`should have as title 'practica04'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('micropractica4');
+    expect(app.title).toEqual('practica04');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('micropractica4 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('practica04 app is running!');
   });
 });
